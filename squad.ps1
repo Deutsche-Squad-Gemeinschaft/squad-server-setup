0a25429e2775ad2e675604a738a6ece92974aee9
Patrick <privat@skyraptor.eu>
2023-05-01 19:58:01 +0200

    Update squad.ps1

--- squad.ps1
+++ squad.ps1
@@ -1,1 +1,16 @@
-
+# Installation:
+# 
+# Copy the script into any directory that is in your PATH variable. 
+# You can call it from PS using the file name i.e. squad.
+#
+# You can find more information on how to edit the PATH and other 
+# environment variables on the offical Microsoft documentation:
+# https://learn.microsoft.com/en-us/windows/win32/procthread/environment-variables
+#
+# Usage:
+#  > squad -instance public start
+#  > squad -instance public stop
+#  > squad -instance public restart
+#
+# Author: Deutsche Squad Gemeinschaft
+# License:
