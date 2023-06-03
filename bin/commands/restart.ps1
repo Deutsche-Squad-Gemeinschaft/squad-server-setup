5415fc50a897d44370cd107fa506456ad3a1dce9
Patrick Henninger <privat@skyraptor.eu>
2023-06-03 15:05:25 +0000

    [Add] restart sub-command

---
+++ bin/commands/restart.ps1
@@ -0,0 +1,13 @@
+using module '../../src/service/service.psm1'
+
+PARAM (
+  [string] $instance
+)
+
+# Verify the instance configuration directory does exist
+if (Test-Path -Path $this.Directory($instance)) {
+    throw "Instance $instance does already exist!"
+}
+
+# Create the instance
+[Service]::Restart($instance)
